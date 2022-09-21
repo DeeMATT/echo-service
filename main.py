8cdebbd29176da5a7f0e01f1e12aa81b87408d93
codemask <mattode@outlook.com>
2022-09-21 19:01:31 +0100

    main .py

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+
+from fastapi import Depends
+from echo_service.core import config, Settings
+
+
+def main(settings: Settings = Depends(config)):
+    uvicorn.run(
+        app="echo_service.core:app",
+        host=config.APP_HOST,
+        port=config.APP_PORT,
+        reload=True if config.ENV != "production" else False,
+        workers=1,
+    )
+
+
+if __name__ == "__main__":
+    main()
